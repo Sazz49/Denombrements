175d89de2a14ad0fb0d039717aef2c65f2710654
lsbry <lsbry@SAZZ_COMPUTER>
2024-10-09 17:30:37 +0200

    optimisation + nouvelle fonction

--- Denombrements/Program.cs
+++ Denombrements/Program.cs
@@ -8,28 +8,44 @@ namespace Denombrements
 {
     class Program
     {
+        static long fact(int n)
+        {
+            if (n < 0)
+            {
+                return 0;
+            }
+            if ((n == 1) || (n==0))
+            {
+                return 1;
+            }
+            else
+            {
+               return n * fact(n - 1);
+            }
+        }
         static void Main(string[] args)
         {
             int c = 1;
             while (c != 0)
             {
+                do
+                {
                 Console.WriteLine("Permutation ...................... 1");
                 Console.WriteLine("Arrangement ...................... 2");
                 Console.WriteLine("Combinaison ...................... 3");
                 Console.WriteLine("Quitter .......................... 0");
                 Console.Write("Choix :                            ");
                 c = int.Parse(Console.ReadLine());
-
-                if (c == 0) { Environment.Exit(0); }
-
+                }
+                while ((c<0) ||(c>3));
+                if (c == 0) 
+                { 
+                    Environment.Exit(0); 
+                }
                 if (c == 1)
-                {
-                    Console.Write("nombre total d'éléments à gérer = "); // le nombre d'éléments à gérer
+                { Console.Write("nombre total d'éléments à gérer = "); // le nombre d'éléments à gérer
                     int n = int.Parse(Console.ReadLine()); // saisir le nombre
-                                                           // calcul de r
-                    long r = 1;
-                    for (int k = 1; k <= n; k++)
-                        r *= k;
+                    long r = fact(n);                      // calcul de r
                     Console.WriteLine(n + "! = " + r);
                 }
                 else
@@ -40,12 +56,9 @@ namespace Denombrements
                         int t = int.Parse(Console.ReadLine()); // saisir le nombre
                         Console.Write("nombre d'éléments dans le sous ensemble = "); // le sous ensemble
                         int n = int.Parse(Console.ReadLine()); // saisir le nombre
-                        // calcul de r
-                        long r = 1;
-                        for (int k = (t - n + 1); k <= t; k++)
-                            r *= k;
-                        //Console.WriteLine("résultat = " + (r1 / r2));
-                        Console.WriteLine("A(" + t + "/" + n + ") = " + r);
+                        long r = fact(t) / fact(t-n); // calcul de r
+
+                        Console.WriteLine("A(" + t + "/" + n + ") = " + r); //Console.WriteLine("résultat = " + (r1 / r2));
                     }
                     else
                     {
@@ -53,17 +66,9 @@ namespace Denombrements
                         int t = int.Parse(Console.ReadLine()); // saisir le nombre
                         Console.Write("nombre d'éléments dans le sous ensemble = "); // le sous ensemble
                         int n = int.Parse(Console.ReadLine()); // saisir le nombre
-                        // calcul de r1
-                        long r1 = 1;
-                        for (int k = (t - n + 1); k <= t; k++)
-                            r1 *= k;
-                        // calcul de r2
-                        long r2 = 1;
-                        for (int k = 1; k <= n; k++)
-                            r2 *= k;
-                        // calcul de r3
-                        //Console.WriteLine("résultat = " + (r1 / r2));
-                        Console.WriteLine("C(" + t + "/" + n + ") = " + (r1 / r2));
+                        long r1 = fact(t);// calcul de r1                       
+                        long r2 = fact(t-n)*fact(n);// calcul de r2               
+                        Console.WriteLine("C(" + t + "/" + n + ") = " + (r1 / r2)); //Console.WriteLine("résultat = " + (r1 / r2));
                     }
                 }
             }
